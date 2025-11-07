9acfd6e6bf8416256c0f9465e5afe08cc9ebadf8
Hyeon54 <hyeon00843@gmail.com>
2025-11-07 19:28:54 +0900

    Update1 main.py-by me

--- main.py
+++ main.py
@@ -13,21 +13,21 @@ def ok():
     msg={"msg":"OK"}
     return msg
 
-"""
-API     : 클라이언트가 요청할 수 있는 하나의 기능(주소 + 요청방식(메서드 get post..)
-FastAPI : 웹 앱으로 API를 만들 수 있게 해줌
-uvicorn : 웹 서버로 requset response를 FastAPI에 전달 (API를 실행시켜주는 서버프로그램임)
 
-웹서버 실행 시킬 때 : uvicorn main:app 
-uvicorn -> 서버야
-main    -> main.py 파일
-app     -> py 파일 안에 있는 FastAPI 인스턴스 이름. app객체 실행시켜라
+# API     : 클라이언트가 요청할 수 있는 하나의 기능(주소 + 요청방식(메서드 get post..)
+# FastAPI : 웹 앱으로 API를 만들 수 있게 해줌
+# uvicorn : 웹 서버로 requset response를 FastAPI에 전달 (API를 실행시켜주는 서버프로그램임)
 
-실행시 나오는 주소 : http://127.0.0.1:8000
-http://127.0.0.1   -> localhost 내pc
-:8000              -> 포트 번호 (컴터 내 돌고 있는 서비스 번호)
+# 웹서버 실행 시킬 때 : uvicorn main:app 
+# uvicorn -> 서버야
+# main    -> main.py 파일
+# app     -> py 파일 안에 있는 FastAPI 인스턴스 이름. app객체 실행시켜라
 
-웹서버주소/docs : API목록과 정보 볼 수 있음
+# 실행시 나오는 주소 : http://127.0.0.1:8000
+# http://127.0.0.1   -> localhost 내pc
+# :8000              -> 포트 번호 (컴터 내 돌고 있는 서비스 번호)
 
-서버 종료 : ctrl + c
-"""
+# 웹서버주소/docs : API목록과 정보 볼 수 있음
+
+# 서버 종료 : ctrl + c
+
